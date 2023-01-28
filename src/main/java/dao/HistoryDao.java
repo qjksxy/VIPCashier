1752296639f04385ecb9a6fcfea2d19df4d02585
Apin <81305669+qjksxy@users.noreply.github.com>
2023-01-28 13:37:41 +0800

    feat(dao): ✨ 增加 History 表的查询方法
    - 增加 History 表根据时间、姓名、电话查询方法

--- src/main/java/dao/HistoryDao.java
+++ src/main/java/dao/HistoryDao.java
@@ -1,10 +1,14 @@
 package dao;
 
+import pojo.Custom;
 import pojo.History;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HistoryDao {
     public static boolean exists() {
@@ -60,5 +64,97 @@ public class HistoryDao {
             return false;
         }
         return true;
+    }
+
+    public static List<History> selectHistoryByPhone(String phone) {
+        Connection c;
+        Statement stmt;
+        List<History> histories = new ArrayList<>();
+        History history = null;
+        try {
+            c = MyDBHelper.getConnection();
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM HISTORY WHERE PHONE LIKE '%" + phone + "%';");
+            while (rs.next()) {
+                // int id, String phone, String subject, String name, int money, long date, String remark
+                history = new History();
+                history.setId(rs.getInt("ID"));
+                history.setPhone(rs.getString("PHONE"));
+                history.setName(rs.getString("NAME"));
+                history.setSubject(rs.getString("SUBJECT"));
+                history.setMoney(rs.getInt("MONEY"));
+                history.setDate(rs.getLong("DATE"));
+                history.setRemark(rs.getString("REMARK"));
+                histories.add(history);
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        return histories;
+
+    }
+
+    public static List<History> selectHistoryByDate(long startTime, long endTime) {
+        Connection c;
+        Statement stmt;
+        List<History> histories = new ArrayList<>();
+        History history = null;
+        try {
+            c = MyDBHelper.getConnection();
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM HISTORY WHERE DATE >=" + startTime + " AND DATE <= " + endTime + ";");
+            while (rs.next()) {
+                history = new History();
+                history.setId(rs.getInt("ID"));
+                history.setPhone(rs.getString("PHONE"));
+                history.setName(rs.getString("NAME"));
+                history.setSubject(rs.getString("SUBJECT"));
+                history.setMoney(rs.getInt("MONEY"));
+                history.setDate(rs.getLong("DATE"));
+                history.setRemark(rs.getString("REMARK"));
+                histories.add(history);
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        return histories;
+
+    }
+
+    public static List<History> selectHistoryByName(String name) {
+        Connection c;
+        Statement stmt;
+        List<History> histories = new ArrayList<>();
+        History history = null;
+        try {
+            c = MyDBHelper.getConnection();
+            stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM HISTORY WHERE NAME LIKE '%" + name + "%';");
+            while (rs.next()) {
+                // int id, String phone, String subject, String name, int money, long date, String remark
+                history = new History();
+                history.setId(rs.getInt("ID"));
+                history.setPhone(rs.getString("PHONE"));
+                history.setName(rs.getString("NAME"));
+                history.setSubject(rs.getString("SUBJECT"));
+                history.setMoney(rs.getInt("MONEY"));
+                history.setDate(rs.getLong("DATE"));
+                history.setRemark(rs.getString("REMARK"));
+                histories.add(history);
+            }
+            rs.close();
+            stmt.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        return histories;
+
     }
 }
