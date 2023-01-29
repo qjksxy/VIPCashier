920bd52b3ab84540ac63cc29b2dab7c278fb0a2c
Apin <81305669+qjksxy@users.noreply.github.com>
2023-01-29 15:25:37 +0800

    test: ✅ HistoryDao 相关测试

--- src/main/test/pojo/BookTest.java
+++ src/main/test/pojo/BookTest.java
@@ -35,23 +35,27 @@ public class BookTest {
 
     @Test
     public void dbTest() {
-//        CustomDao.createTable();
-//        Custom custom = new Custom("13843468583", "战三", 10000, 10000, "100-10", "");
-//        CustomDao.insertCustom(custom);
-//        List<Custom> customs = CustomDao.selectCustomByPhone("138");
-//        System.out.println(customs);
-        if (!HistoryDao.exists()) {
-            HistoryDao.createTable();
-        }
-        History history = new History();
-        history.setId(1);
-        history.setName("张三");
-        history.setPhone("10000000000");
-        history.setMoney(1000);
-        history.setSubject("剪发");
-        history.setDate(new Date().getTime());
-        history.setRemark("会员");
+       // CustomDao.createTable();
+       // Custom custom = new Custom("13843468583", "战三", 10000, 10000, "100-10", "");
+       // CustomDao.insertCustom(custom);
+       // List<Custom> customs = CustomDao.selectCustomByPhone("138");
+       // System.out.println(customs);
+       //  if (!HistoryDao.exists()) {
+       //      HistoryDao.createTable();
+       //  }
+       //  History history = new History();
+       //  history.setName("王五");
+       //  history.setPhone("10000000000");
+       //  history.setMoney(1500);
+       //  history.setSubject("剪发");
+       //  history.setDate(new Date().getTime());
+       //  history.setRemark("");
+       //
+       //  HistoryDao.insertHistory(history);
+       //  List<History> histories = HistoryDao.selectHistoryByDate(1674879750073L, 1674879946997L);
+       //  System.out.println(histories);
 
-        HistoryDao.insertHistory(history);
+        List<History> histories = HistoryDao.selectHistoryByName("王");
+        System.out.println(histories);
     }
 }
